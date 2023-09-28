54bf94b4860cef45bcc25b0d62a98d501c5d8037
JoergWolff <neuerfisch@wolffclan.de>
2023-09-28 18:09:03 +0200

    add searching and errormessage

--- src/assets/gallery/Galery.tsx
+++ src/assets/gallery/Galery.tsx
@@ -1,6 +1,7 @@
 import './Galary.css'
 import {RickAndMorty} from "../../helper/database/model.tsx";
 import Card from "../../card/Card.tsx";
+import {ChangeEvent, useState} from "react";
 
 type GalleryProps = {
     items: RickAndMorty[]
@@ -8,11 +9,22 @@ type GalleryProps = {
 
 export default function Gallery(props: GalleryProps) {
 
+    const [searchText, setSearchText]=useState<string>("")
+
+    const searchCharacters = props.items.filter(character=>character.name.toLowerCase().includes(searchText.toLowerCase()))
+
+    function onSearch(event: ChangeEvent<HTMLInputElement>){
+        setSearchText(event.target.value)
+    }
 
     return (
         <>
-            <header className="gallery_header"><h2>Gallery</h2><input type="search" placeholder="Search"/></header>
-            {props.items.map(item => <Card key={item.id} newItem={item}/>)}
+            <header className="gallery_header"><h2>Gallery</h2><input type="search" placeholder="Search" value={searchText} onChange={onSearch}/></header>
+
+            {searchCharacters.length === 0 ?
+                <h2>Leider kein Eintrag gefunden...</h2>
+                :
+                searchCharacters.map(item => <Card key={item.id} newItem={item}/>) }
         </>
 
     )
